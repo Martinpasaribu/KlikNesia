4e08d7bbbe8cc738580cf6680d2379154249fa37
Anugera Rejeki Abadi <martin12082000@gmail.com>
2025-05-31 23:49:58 +0700

    update domain dan google anallis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Providers } from "@/store/provider";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Layout from "@/components/Layout";
+import Script from "next/script";
+import { Toaster } from "react-hot-toast";
+
 const geistSans = Geist({
   subsets: ["latin"],
   variable: "--font-geist-sans",
@@ -15,9 +18,50 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "My App",
-  description: "A Next.js app with SSR and dark mode",
+  title: "Click Usaha",
+  description:
+    "Click Usaha adalah solusi digital lengkap untuk UMKM dan bisnis Indonesia. Kami menyediakan layanan pengembangan website, aplikasi mobile, branding, dan desain kreatif untuk mendukung pertumbuhan bisnis Anda.",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/x-icon",
+      sizes: "32x32",
+      url: "https://clickusaha.com/favicon.ico",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "https://clickusaha.com/apple-icon.png",
+    },
+  ],
+  keywords:
+    "Click Usaha, Jasa pengembangan website UMKM, Aplikasi mobile bisnis, Branding digital Indonesia, Desain kreatif untuk usaha",
+  openGraph: {
+    title: "ClickUsaha",
+    description:
+      "Click Usaha adalah solusi digital lengkap untuk UMKM dan bisnis Indonesia. Temukan layanan kami mulai dari pembuatan website hingga branding bisnis.",
+    url: "https://www.clickusaha.com",
+    siteName: "ClickUsaha",
+    images: [
+      {
+        url: "https://clickusaha.com/og-image.png", // ganti dengan OG image asli kamu
+        width: 1200,
+        height: 630,
+        alt: "ClickUsaha - Solusi Digital UMKM",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ClickUsaha",
+    description:
+      "Click Usaha menyediakan layanan digital modern untuk usaha Anda. Website, aplikasi mobile, branding, dan lainnya.",
+    images: ["https://clickusaha.com/og-image2.png"], // ganti jika sudah punya gambar
+  },
 };
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -27,6 +71,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       {/* 👆 suppressHydrationWarning membantu Next menghindari warning */}
       <body className={`${geistSans.variable} ${geistMono.variable} w-full`}>
+
+        {/* Google tag (gtag.js) */}
+        <Script 
+            async 
+            src="https://www.googletagmanager.com/gtag/js?id=G-78LPKBXL1J"
+        />
+        <Script id="google-analytics">
+          { ` window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-78LPKBXL1J');
+          `}
+        </Script>
+        <Toaster position="top-center" />
         <Providers>
           <ThemeProvider>
             <Layout>
